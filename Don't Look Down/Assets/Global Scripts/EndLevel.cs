1ae8c0e96f0a2bc6bc90495e1908fe48b72799da
Aneesh Goel <aneesh.goel.rbtx@gmail.com>
2015-07-27 23:43:24 -0700

    Menu and basic scene 1

---
+++ Don't Look Down/Assets/Global Scripts/EndLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+	
+	void OnTriggerEnter (Collider other)
+	{
+		GameObject otherObject = other.gameObject;
+		if (otherObject.CompareTag ("endlevel")) {
+			Application.LoadLevel(0);
+		}
+	}
+}
---
+++ Don't Look Down/Assets/_Scenes/Menu Screen/Scripts/ExitOnClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitOnClick : MonoBehaviour {
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
