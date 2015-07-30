4521ff2e1393d689ea21438ec0b651c7e4028d4c
Josh Wien <jwien001@gmail.com>
2015-07-30 16:23:38 -0700

    Add beep block level

---
+++ Don't Look Down/Assets/_Scenes/jwien/jwien2_beep_block_insert_pun_here/TogglePlatforms.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePlatforms : MonoBehaviour {
+
+	[SerializeField] private GameObject m_RedPlatforms;
+	[SerializeField] private GameObject m_BluePlatforms;
+	[SerializeField] private float m_ToggleTime;
+
+	[SerializeField] private AudioClip m_BeepClip;
+	[SerializeField] private AudioClip m_SlamClip;
+	[SerializeField] private float m_SoundInterval;
+	[SerializeField] private int m_SoundRepeat;
+
+	private AudioSource m_AudioSource;
+	private bool showBlue = false;
+
+	// Use this for initialization
+	void Start () {
+		m_AudioSource = GetComponent<AudioSource>();
+		ActivatePlatforms();
+		float waitTime = m_ToggleTime - (m_SoundInterval * m_SoundRepeat);
+		StartCoroutine(DoIt(waitTime));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator DoIt(float toggleTime) {
+		while (true) {
+			yield return new WaitForSeconds(toggleTime);
+			//ActivatePlatforms();
+			for (int i = 0; i < m_SoundRepeat - 1; i++) {
+				m_AudioSource.PlayOneShot(m_BeepClip);
+				yield return new WaitForSeconds(m_SoundInterval);
+			}
+			m_AudioSource.PlayOneShot(m_SlamClip);
+			showBlue = !showBlue;
+			ActivatePlatforms();
+		}
+	}
+
+	void ActivatePlatforms() {
+		m_RedPlatforms.SetActive(!showBlue);
+		m_BluePlatforms.SetActive(showBlue);
+	}
+}
