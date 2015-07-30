bf79d63f09000efb40712802a02e1b3a2545dcef
Aneesh Goel <aneesh.goel.rbtx@gmail.com>
2015-07-29 22:36:23 -0700

    Player controller + demo levels
    1) Remove jumping properly
    2) Falling is conditional on looking down
    3) Add a few demo levels
    4) Add some more useful prefabs

--- Don't Look Down/Assets/Global Scripts/EndLevel.cs
+++ Don't Look Down/Assets/Global Scripts/EndLevel.cs
@@ -6,8 +6,11 @@ public class EndLevel : MonoBehaviour {
 	void OnTriggerEnter (Collider other)
 	{
 		GameObject otherObject = other.gameObject;
-		if (otherObject.CompareTag ("endlevel")) {
-			Application.LoadLevel(0);
+		if (otherObject.CompareTag ("winLevel")) {
+			// If we're on the last level, wraps back to 0 (menu)
+			Application.LoadLevel((Application.loadedLevel + 1) % Application.levelCount);
+		} else if (otherObject.CompareTag ("fallToDeath")) {
+			Application.LoadLevel (Application.loadedLevel);
 		}
 	}
 }
---
+++ Don't Look Down/Assets/Global Scripts/TextTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTimer : MonoBehaviour {
+
+	public float displaySeconds;
+	private float displaySecondsLeft;
+
+	// Use this for initialization
+	void Start () {
+		displaySecondsLeft = displaySeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		displaySecondsLeft -= Time.deltaTime;
+		if (displaySecondsLeft <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
