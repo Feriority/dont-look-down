1519c9ef3c49092eb3d8011f60aaca9d2860bb45
yodprotan <achi@yelp.com>
2015-07-31 14:50:07 -0700

    nightfall finished

---
+++ Don't Look Down/Assets/Global Scripts/Inactivate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inactivate : MonoBehaviour {
+
+	[SerializeField] private GameObject m_objects;
+
+	// Use this for initialization
+	void Start () {
+		m_objects.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
